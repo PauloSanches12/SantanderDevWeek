b30e3d80948fa739d580c4f3e1ca459b38558a3c
Paulo <paulo_sanches2012@hotmail.com>
2021-05-26 15:01:41 -0300

    created card

--- app/src/main/java/com/paulo/santanderdevweek/MainActivity.kt
+++ app/src/main/java/com/paulo/santanderdevweek/MainActivity.kt
@@ -2,18 +2,34 @@ package com.paulo.santanderdevweek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        setSupportActionBar(toolbar)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflate = menuInflater
         inflate.inflate(R.menu.main_menu, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_1 -> {
+                Log.d("Click", "Click no item 1")
+                true
+            }
+            else -> {
+                Log.d("Click", "Click no item 2")
+                true
+            }
+        }
     }
 }
